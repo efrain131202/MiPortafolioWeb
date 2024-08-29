6458901533718e68982abc603bd79656fdd07410
Efrain131202 <efraincruzlobato13@gmail.com>
2024-08-28 20:44:54 -0600

    Agregando cambios a nivel global y agregando bio

---
+++ src/scripts/cards.js
@@ -0,0 +1,26 @@
+export const cards = [
+    {
+        title: "Flutter",
+        imageSrc: "/Icons/MaterialSymbolsFlutter.svg",
+    },
+    {
+        title: "Astro",
+        imageSrc: "/Icons/SimpleIconsAstro.svg",
+    },
+    {
+        title: "Git",
+        imageSrc: "/Icons/SimpleIconsGit.svg",
+    },
+    {
+        title: "Node.js",
+        imageSrc: "/Icons/TeenyiconsNodejsSolid.svg",
+    },
+    {
+        title: "Tailwind CSS",
+        imageSrc: "/Icons/MdiTailwind.svg",
+    }
+];
+
+export const bio = `
+Soy Efraín Cruz Lobato, ingeniero en Desarrollo y Gestión de Software graduado de la Universidad Tecnológica de Tlaxcala. Tengo experiencia en el desarrollo multiplataforma utilizando Dart y Flutter, así como en desarrollo web usando Astro, HTML, CSS, Tailwind, Node.js y JavaScript. Además, estoy familiarizado con herramientas de gestión de proyectos, asegurando una ejecución organizada. Estoy comprometido con la excelencia en el desarrollo de software y busco constantemente mejorar mis habilidades.
+`;
