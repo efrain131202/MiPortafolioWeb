631f42e38c8e1e1a650e50678d0e0f82b26afa1d
Efrain131202 <efraincruzlobato13@gmail.com>
2024-08-29 11:32:08 -0600

    Agregando extras y cambiando tipo de letra

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -24,3 +24,74 @@ export const cards = [
 export const bio = `
 Soy Efraín Cruz Lobato, ingeniero en Desarrollo y Gestión de Software graduado de la Universidad Tecnológica de Tlaxcala. Tengo experiencia en el desarrollo multiplataforma utilizando Dart y Flutter, así como en desarrollo web usando Astro, HTML, CSS, Tailwind, Node.js y JavaScript. Además, estoy familiarizado con herramientas de gestión de proyectos, asegurando una ejecución organizada. Estoy comprometido con la excelencia en el desarrollo de software y busco constantemente mejorar mis habilidades.
 `;
+
+export const intro = `
+    Además de mis habilidades principales, también tengo conocimientos en OSINT, Ciberseguridad Básica, y Gestión de Software, que refuerzan mi perfil profesional.
+`;
+
+export const extra = [
+    {
+        title: "OSINT",
+        imageSrc: "/Icons/HugeiconsComputerCloud.svg",
+    },
+    {
+        title: "Ciberseguridad Básica",
+        imageSrc: "/Icons/HugeiconsComputerProtection.svg",
+    },
+    {
+        title: "Gestión de Software",
+        imageSrc: "/Icons/HugeiconsComputerProgramming02.svg",
+    }
+];
+
+
+
+export const projects = [
+    {
+        title: "Telmex_App",
+        description: "App para visualizar datos de Excel",
+        technologies: "Dart, Flutter",
+        imageSrc: "/images/telmex_app.png"
+    },
+    {
+        title: "Telegram_Bot_Telmex",
+        description: "Bot para consultas desde Excel",
+        technologies: "Node.js, JavaScript",
+        imageSrc: "/images/telegram_bot_telmex.png"
+    },
+    {
+        title: "Vial-Solutions",
+        description: "Landing page de una app",
+        technologies: "Astro, HTML, Tailwind, JavaScript",
+        imageSrc: "/images/vial_solutions.png",
+        website: "https://vialsolutions.netlify.app/"
+    },
+    {
+        title: "Hotel Boutique Alqueria Central",
+        description: "Landing page de Hotel",
+        technologies: "Astro, HTML, CSS, JavaScript",
+        imageSrc: "/images/alqueria_central.png",
+        website: "https://667c61ea6a1f7f000860cebb--alqueriacentral.netlify.app/"
+    },
+    {
+        title: "Tlaxco de mi Corazón",
+        description: "Landing page de un restaurante",
+        technologies: "Astro, HTML, CSS, JavaScript",
+        imageSrc: "/images/tlaxco_corazon.png",
+        website: "https://tlaxco-de-mi-corazon.netlify.app/"
+    },
+    {
+        title: "Electronic Ecommerce Landing Page",
+        description: "Landing page de una Ecommerce",
+        technologies: "Astro, React.js, HTML, Tailwind",
+        imageSrc: "/images/electronic_ecommerce.png",
+        website: "https://electronic-ecommerce-landig-page.netlify.app/"
+    },
+    {
+        title: "Cafe Negro Que Me Alegro",
+        description: "Landing page de un restaurante",
+        technologies: "HTML, CSS, JavaScript",
+        imageSrc: "/images/cafe_negro.png",
+        website: "https://cafe-negro-que-me-alegro.netlify.app/"
+    }
+];
