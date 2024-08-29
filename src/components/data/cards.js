c5f70f9bc7052ce819cfa0b5361d101c12af832d
Efrain131202 <efraincruzlobato13@gmail.com>
2024-08-28 18:26:43 -0600

    Agregando pagina de tecnologias

---
+++ src/components/data/cards.js
@@ -0,0 +1,32 @@
+export const cards = [
+    {
+        title: "Flutter",
+        description: "Crea apps para iOS y Android con un solo código.",
+        imageSrc: "/Icons/MaterialSymbolsFlutter.svg",
+    },
+    {
+        title: "Astro",
+        description: "Construye sitios web rápidos con JavaScript, HTML y CSS.",
+        imageSrc: "/Icons/SimpleIconsAstro.svg",
+    },
+    {
+        title: "Git",
+        description: "Controla y rastrea cambios en tu código.",
+        imageSrc: "/Icons/SimpleIconsGit.svg",
+    },
+    {
+        title: "Node.js",
+        description: "Ejecuta JavaScript en el servidor, rápido y escalable.",
+        imageSrc: "/Icons/TeenyiconsNodejsSolid.svg",
+    },
+    {
+        title: "React",
+        description: "Crea interfaces web interactivas y dinámicas.",
+        imageSrc: "/Icons/SimpleIconsReact.svg",
+    },
+    {
+        title: "Tailwind CSS",
+        description: "Diseña rápido con clases CSS predefinidas.",
+        imageSrc: "/Icons/MdiTailwind.svg",
+    }
+];
